5c4b5acc5a093564ae34fc5cf054590a3d2b59d7
Hihhh-lilyJ <eggylilwin@gmail.com>
2023-03-08 09:35:19 +0630

    Initial Commit

---
+++ Caculator/CaculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaculatorApp.swift
+//  Caculator
+//
+//  Created by MacBook Pro on 07/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct CaculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
